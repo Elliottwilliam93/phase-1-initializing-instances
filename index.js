365dae0d3e186a5bd6ff900af336b75e42745e6f
williamelliott <ironmaiden229@hotmail.com>
2023-05-05 13:30:23 -0400

    project done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food
+        this.drink = drink
+    }
+}
+
+let breakfast = new Breakfast
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+let lunch = new Lunch
+
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.#dessert = dessert
+    }
+}
+
+let dinner = new Dinner
